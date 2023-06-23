d3eb7823cc3fc030fdeb73e2884f06304c74c914
CarlosCA94 <cw.21_94@hotmail.com>
2023-06-23 00:24:02 -0700

    Script Program C - Print using Printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,22 @@
+/*
+ * Printf
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - lastwords
+ *
+ * Description: print use command printf
+ *
+ * Return: zero
+ */
+
+int main(void)
+
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
