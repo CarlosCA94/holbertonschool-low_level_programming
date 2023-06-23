379a5a86cdd9fe859a864faa6ec9c0471e9e61ef
CarlosCA94 <cw.21_94@hotmail.com>
2023-06-23 00:02:18 -0700

    Script C Program - Print

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - first program
+ *
+ * Description - write on the screen
+ *
+ * Return: zero on success
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
