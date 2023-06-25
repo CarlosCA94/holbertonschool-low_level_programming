d8699da5791715e79cb3cd763e714bf9d3601af3
CarlosCA94 <cw.21_94@hotmail.com>
2023-06-25 02:44:44 -0700

    Prototype

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - first program
+ *
+ * Description - write on the screen
+ *
+ * Return: zero
+ */
+
+int main(void)
+
+{
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
