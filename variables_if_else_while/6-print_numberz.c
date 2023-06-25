9be6a81ec323fc7703a38321f124b0453591808a
CarlosCA94 <cw.21_94@hotmail.com>
2023-06-25 01:45:21 -0700

    Program C - Print digits of base 10 using putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - first program
+ *
+ * Description - write on the screen
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+int b;
+
+for (b = 0 ; b < 10 ; b++)
+putchar(b + '0');
+
+putchar('\n');
+
+return (0);
+}
