005d50f23d4c63ac241e54896db3b01aae5dea64
CarlosCA94 <cw.21_94@hotmail.com>
2023-06-25 11:55:25 -0700

    Prototype - Check for uppercase letter

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isalpha - Check for lowercase character.
+*
+* @c: int
+*
+* Return: int c
+*/
+
+int _isalpha(int c)
+
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
