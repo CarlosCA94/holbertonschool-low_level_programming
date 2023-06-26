24018dcbaa59df46c597211fd07890acd05a5381
CarlosCA94 <cw.21_94@hotmail.com>
2023-06-25 19:45:58 -0700

    Program - Value of last digit of a number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_last_digit - Prints the last digit of a number.
+*
+* @n: int
+*
+* Return: int n
+*/
+
+int print_last_digit(int n)
+{
+
+return (n % 10);
+return (0);
+
+}
