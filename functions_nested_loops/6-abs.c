f82e0b104d25bcc7a3cadf531bb024a5b27d90fb
CarlosCA94 <cw.21_94@hotmail.com>
2023-06-25 19:29:33 -0700

    Program - Absolute value for a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _abs - Absolute value of an integer.
+*
+* @n: int
+*
+* Return: int n
+*/
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
