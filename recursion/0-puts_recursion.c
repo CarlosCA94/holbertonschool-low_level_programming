9d7558bfd55bb5a4b4bf5d6f928696e7e4e764a1
CarlosCA94 <cw.21_94@hotmail.com>
2023-07-02 19:27:00 -0700

    Functions - Prints a string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string.
+ * @s: int
+ *
+ * Return: inc s.
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
+}
