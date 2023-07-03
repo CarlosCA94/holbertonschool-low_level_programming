1101662ff220104a0a8c0f4edfb78648bcb9d4ce
CarlosCA94 <cw.21_94@hotmail.com>
2023-07-02 21:53:02 -0700

    Reupload - Functions that duplicates a string

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,27 +10,28 @@
 
 char *_strdup(char *str)
 {
-unsigned int i = 0;
-unsigned int n = 0;
-char *y = malloc(sizeof(char) * (i + 1));
+int a = 0, i = 1;
+char *s;
 
 if (str == NULL)
-{
 return (NULL);
-}
 
 while (str[i])
-i++;
-
-if (y == NULL)
-return (NULL);
-
-while (str[n])
 {
-y[n] = str[n];
-n++;
+i++;
 }
 
-y[n + 1] = 0;
-return (y);
+s = malloc((sizeof(char) * i) +1);
+
+if (s == NULL)
+return (NULL);
+
+while (a < i)
+{
+s[a] = str[a];
+a++;
+}
+
+s[a] = '\0';
+return (s);
 }
